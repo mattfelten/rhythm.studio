a9c88785ddbe5c9946ced3fc04f01316ce96c7fb
Matt Felten <m@mattfelten.com>
2025-07-10 16:51:05 -0700

    First draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://rhythm.studio',
+
+	integrations: [mdx()],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	theme: {
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						'--tw-prose-body': 'var(--color-subdued)',
+						'--tw-prose-headings': 'var(--color-primary)',
+						'--tw-prose-lead': 'var(--color-primary)',
+						'--tw-prose-links': 'var(--color-primary)',
+						'--tw-prose-bold': 'var(--color-primary)',
+						'--tw-prose-counters': 'var(--color-primary)',
+						'--tw-prose-bullets': 'var(--color-primary)',
+						'--tw-prose-hr': 'var(--color-primary)',
+						'--tw-prose-quotes': 'var(--color-primary)',
+						'--tw-prose-quote-borders':
+							'var(--color-primary)',
+						'--tw-prose-captions': 'var(--color-primary)',
+						'--tw-prose-code': 'var(--color-primary)',
+						'--tw-prose-pre-code': 'var(--color-primary)',
+						'--tw-prose-pre-bg': 'var(--color-primary)',
+						'--tw-prose-th-borders':
+							'var(--color-primary)',
+						'--tw-prose-td-borders':
+							'var(--color-primary)',
+						'--tw-prose-invert-body':
+							'var(--color-background)',
+						'--tw-prose-invert-headings':
+							'var(--color-background)',
+						'--tw-prose-invert-lead':
+							'var(--color-background)',
+						'--tw-prose-invert-links':
+							'var(--color-background)',
+						'--tw-prose-invert-bold':
+							'var(--color-background)',
+						'--tw-prose-invert-counters':
+							'var(--color-background)',
+						'--tw-prose-invert-bullets':
+							'var(--color-background)',
+						'--tw-prose-invert-hr':
+							'var(--color-background)',
+						'--tw-prose-invert-quotes':
+							'var(--color-background)',
+						'--tw-prose-invert-quote-borders':
+							'var(--color-background)',
+						'--tw-prose-invert-captions':
+							'var(--color-background)',
+						'--tw-prose-invert-code':
+							'var(--color-background)',
+						'--tw-prose-invert-pre-code':
+							'var(--color-background)',
+						'--tw-prose-invert-pre-bg':
+							'rgb(0 0 0 / 50%)',
+						'--tw-prose-invert-th-borders':
+							'var(--color-background)',
+						'--tw-prose-invert-td-borders':
+							'var(--color-background)',
+					},
+				},
+			},
+		},
+	},
+};
